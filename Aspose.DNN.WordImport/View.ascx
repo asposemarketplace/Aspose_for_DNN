cdd09b7e780dde1090dd7b5dd46133e669584476
Aspose <marketplace@aspose.com>
2014-04-27 12:22:33 +0500

    Initial commit for DNN Word Import

---
+++ Aspose.DNN.WordImport/View.ascx
@@ -0,0 +1,12 @@
+﻿<%@ Control Language="C#" AutoEventWireup="true" CodeBehind="View.ascx.cs" Inherits="Modules.Aspose.DNN.WordImport.View" %>
+
+<div id="div_main">
+    <br />
+    <asp:Literal ID="OutputLiteral" runat="server"></asp:Literal>
+    <br />    
+    <asp:FileUpload ID="ImportFileUpload" runat="server" />
+    <br />
+    <br />
+    <asp:Button ID="ImportButton" runat="server" Text="Import from Word" OnClick="ImportButton_Click" />
+    <br />
+</div>
---
+++ Aspose.DNN.WordImport/View.ascx.cs
@@ -0,0 +1,94 @@
+﻿/*
+' Copyright (c) 2014  Christoc.com
+'  All rights reserved.
+' 
+' THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED
+' TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+' THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF
+' CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+' DEALINGS IN THE SOFTWARE.
+' 
+*/
+
+using System;
+using DotNetNuke.Security;
+using DotNetNuke.Services.Exceptions;
+using DotNetNuke.Entities.Modules;
+using DotNetNuke.Entities.Modules.Actions;
+using DotNetNuke.Services.Localization;
+using System.IO;
+using Aspose.Words;
+
+namespace Modules.Aspose.DNN.WordImport
+{
+    /// -----------------------------------------------------------------------------
+    /// <summary>
+    /// The View class displays the content
+    /// 
+    /// Typically your view control would be used to display content or functionality in your module.
+    /// 
+    /// View may be the only control you have in your project depending on the complexity of your module
+    /// 
+    /// Because the control inherits from Aspose.DNN.WordImportModuleBase you have access to any custom properties
+    /// defined there, as well as properties from DNN such as PortalId, ModuleId, TabId, UserId and many more.
+    /// 
+    /// </summary>
+    /// -----------------------------------------------------------------------------
+    public partial class View : WordImportModuleBase, IActionable
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            try
+            {
+
+            }
+            catch (Exception exc) //Module failed to load
+            {
+                Exceptions.ProcessModuleLoadException(this, exc);
+            }
+        }
+
+        protected void ImportButton_Click(object sender, EventArgs e)
+        {
+            if (ImportFileUpload.HasFile)
+            {
+                // Check for license and apply if exists
+                string licenseFile = Server.MapPath("~/App_Data/Aspose.Total.lic");
+                if (File.Exists(licenseFile))
+                {
+                    License license = new License();
+                    license.SetLicense(licenseFile);
+                }
+
+                Stream stream = ImportFileUpload.FileContent;
+                Document doc = new Document(stream);
+
+                string filePath = Server.MapPath("~/temp/");
+                if (!Directory.Exists(filePath))
+                    Directory.CreateDirectory(filePath);
+
+                filePath += "\\" + System.Guid.NewGuid().ToString();
+
+                doc.Save(filePath, SaveFormat.Html);
+                string outputText = File.ReadAllText(filePath);
+                OutputLiteral.Text = outputText;
+
+            }
+        }
+
+        public ModuleActionCollection ModuleActions
+        {
+            get
+            {
+                var actions = new ModuleActionCollection
+                    {
+                        {
+                            GetNextActionID(), Localization.GetString("EditModule", LocalResourceFile), "", "", "",
+                            EditUrl(), false, SecurityAccessLevel.Edit, true, false
+                        }
+                    };
+                return actions;
+            }
+        }
+    }
+}
