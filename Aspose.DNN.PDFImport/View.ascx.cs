a86b7c2623242f7e053cf559709b38626902c7ba
Aspose <marketplace@aspose.com>
2014-06-04 14:45:29 +0500

    Initial commit for Aspose DNN PDF Import

---
+++ Aspose.DNN.PDFImport/View.ascx.cs
@@ -0,0 +1,80 @@
+﻿/*
+' Copyright (c) 2014  Christoc.com
+'  All rights reserved.
+' 
+' THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED
+' TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+' THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF
+' CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+' DEALINGS IN THE SOFTWARE.
+' 
+*/
+
+using System;
+using DotNetNuke.Security;
+using DotNetNuke.Services.Exceptions;
+using DotNetNuke.Entities.Modules;
+using DotNetNuke.Entities.Modules.Actions;
+using DotNetNuke.Services.Localization;
+using Aspose.Pdf;
+using System.IO;
+
+namespace Aspose.Modules.AsposeDNNPdfImport
+{
+    public partial class View : AsposeDNNPdfImportModuleBase, IActionable
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            try
+            {
+
+            }
+            catch (Exception exc) //Module failed to load
+            {
+                Exceptions.ProcessModuleLoadException(this, exc);
+            }
+        }
+
+        public ModuleActionCollection ModuleActions
+        {
+            get
+            {
+                var actions = new ModuleActionCollection
+                    {
+                        {
+                            GetNextActionID(), Localization.GetString("EditModule", LocalResourceFile), "", "", "",
+                            EditUrl(), false, SecurityAccessLevel.Edit, true, false
+                        }
+                    };
+                return actions;
+            }
+        }
+
+        protected void ImportButton_Click(object sender, EventArgs e)
+        {
+            if (ImportFileUpload.HasFile)
+            {
+                // Check for license and apply if exists
+                string licenseFile = Server.MapPath("~/App_Data/Aspose.Total.lic");
+                if (File.Exists(licenseFile))
+                {
+                    License license = new License();
+                    license.SetLicense(licenseFile);
+                }
+
+                // Initialize the stream to read the uploaded file.
+                Stream myStream = ImportFileUpload.FileContent;
+                //open document
+                Document pdfDocument = new Document(myStream);
+                string path = Server.MapPath(".") + "//" + ImportFileUpload.FileName.Replace(".pdf", ".html");
+                pdfDocument.Save(path, SaveFormat.Html);
+                string extractedText = File.ReadAllText(path);
+                OutputLiteral.Text = extractedText;
+            }
+            else
+            {
+                OutputLiteral.Text = "Please Upload File";
+            }
+        }
+    }
+}
